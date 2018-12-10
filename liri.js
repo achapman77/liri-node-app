529b5ea9ac81a44a72d15d6d774eb916942c60ea
AChapman <achapman77@gmail.com>
2018-12-09 22:49:23 -0700

    Spotify-this working well.  Added chalk color formatting

--- liri.js
+++ liri.js
@@ -1,14 +1,20 @@
-// require("dotenv").config();
+require("dotenv").config();
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api')
+var spotify = new Spotify(keys.spotify);
 
-var inquirer = require("inquirer");
-var moment = require("moment");
-// var keys = require("./keys.js");
+var inquirer = require('inquirer');
 
-var axios = require("axios");
+var moment = require('moment');
 
-// var omdb = require('omdb');
+var axios = require('axios');
 
-// var spotify = new Spotify(keys.spotify);
+var omdb = require('omdb');
+
+const chalk = require('chalk');
+// const error = chalk.bold.red;
+// const warning = chalk.keyword('orange');
+const log = console.log;
 
 inquirer
   .prompt([
@@ -31,16 +37,16 @@ inquirer
     //Need to add + inbetween user input words to enable API calls
     //Therefore make user input into array
     //loop through all the words in user input and add "+"
-      var userInput = "";
+      var userUrlQuery = "";
     
         var userInputArray = inquirerResponse.userInput.split(" ");
         for (var i = 0; i < userInputArray.length; i++) {
 
             if (i > 0 && i < userInputArray.length) {
-                userInput = userInput + "+" + userInputArray[i];
+                userUrlQuery = userUrlQuery + "+" + userInputArray[i];
             }
             else {
-                userInput += userInputArray[i]
+                userUrlQuery += userInputArray[i]
             }
         };
 
@@ -52,24 +58,47 @@ inquirer
       var command = inquirerResponse.liriCommand;
       switch (command) {
           case "Find Concert for a Band":
-              concertThis(userInput);
+            //   userUrlQuery(inquirerResponse.userInput);
+              concertThis(userUrlQuery);
+              break;
+          case "Spotify a Song":
+              if (inquirerResponse.userInput === "") {
+                  spotifyAceofBase()
+              } else {
+                spotifyThisSong(inquirerResponse.userInput);
+              }
+              
               break;
       }
     
   });
 
+// var userUrlQuery = (userInput) => {
+//     var userUrlQuery = "";
+    
+//     var userInputArray = inquirerResponse.userInput.split(" ");
+//     for (var i = 0; i < userInputArray.length; i++) {
+
+//         if (i > 0 && i < userInputArray.length) {
+//             userUrlQuery = userUrlQuery + "+" + userInputArray[i];
+//         }
+//         else {
+//             userUrlQuery += userInputArray[i]
+//         }
+//     };
+//   }
 
 //concert-this
 // This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
     // Name of the venue
     // Venue location
     // Date of the Event (use moment to format this as "MM/DD/YYYY")
-function concertThis(userInput) {
+function concertThis(userUrlQuery) {
 
-    var reFormatUserInput = userInput.split("+").join(" ").toUpperCase();
+    var reFormatUserInput = userUrlQuery.split("+").join(" ").toUpperCase();
 
     axios
-    .get(`https://rest.bandsintown.com/artists/${userInput}/events?app_id=codingbootcamp`)
+    .get(`https://rest.bandsintown.com/artists/${userUrlQuery}/events?app_id=codingbootcamp`)
     .then(function (response) {
         // console.log(response.data);
         
@@ -80,7 +109,8 @@ function concertThis(userInput) {
 
         // console.log(diffDays);
 
-        console.log(`There's a ${reFormatUserInput} concert at the ${response.data[0].venue.name} in ${response.data[0].venue.city}, ${response.data[0].venue.region} on ${concertDateFormatted}.  Hurry and buy tickets soon...That's only ${diffDays} days from now!`);
+        log(chalk.blue(`\nThere's a ${reFormatUserInput} concert at the ${response.data[0].venue.name} in ${response.data[0].venue.city}, ${response.data[0].venue.region} on ${concertDateFormatted}.\n`) +
+        chalk.red(`Hurry and buy tickets soon...That's only ${diffDays} days from now!\n`));
 
     })
     .catch(function(error) {
@@ -113,8 +143,64 @@ function concertThis(userInput) {
     // The album that the song is from
 // If no song is provided then your program will default to "The Sign" by Ace of Base.
 
+function spotifyThisSong(userInput) {
+    
+    spotify
+        .search({ type: 'track', query: `${userInput}`, limit: 10 })
+        .then(function(response) {
+            // if () {
 
+            // }
+            for (var i = 0; i < response.tracks.items.length; i++) {
+                
+                log(chalk.blue(`\nTRACK: ${response.tracks.items[i].name}`));
+                for (var x = 0; x < response.tracks.items[i].artists.length; x++) {
+                    log(chalk.green(`ARTIST(s): ${response.tracks.items[i].artists[x].name}`));
+                }
+                log(chalk.yellow(`ALBUM: ${response.tracks.items[i].album.name}`));
+                log(`PreVIEW: ${response.tracks.items[i].preview_url}`);
+                log(chalk.red(`--------------------------------------------------`));
+            }
 
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+    
+    // spotify.search({ type: 'track', query: `${userInput}` }, function(err, data) {
+    //     if (err) {
+    //         return console.log('Error occurred: ' + err);
+    //     }
+        
+    //     console.log(data.tracks.items[0]); 
+    //     });
+};
+
+function spotifyAceofBase(userInput) {
+    
+    spotify
+        .search({ type: 'track', query: "The Sign", limit: 10 })
+        .then(function(response) {
+
+            log(chalk.blue(`\nTRACK: ${response.tracks.items[8].name}`));
+            log(chalk.green(`ARTIST(s): ${response.tracks.items[8].artists[0].name}`));
+            log(chalk.yellow(`ALBUM: ${response.tracks.items[8].album.name}`));
+            log(`PreVIEW: ${response.tracks.items[8].preview_url}`);
+            log(chalk.red(`--------------------------------------------------`));
+
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+    
+    // spotify.search({ type: 'track', query: `${userInput}` }, function(err, data) {
+    //     if (err) {
+    //         return console.log('Error occurred: ' + err);
+    //     }
+        
+    //     console.log(data.tracks.items[0]); 
+    //     });
+};
 
 
 //movie-this
